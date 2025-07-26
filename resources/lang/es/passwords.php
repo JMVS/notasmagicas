26c5277041a2297c976cda599bab953f6ebbf25a
JMVS <JMVS@users.noreply.github.com>
2025-07-25 21:22:35 -0300

    Creación en español

---
+++ resources/lang/es/passwords.php
@@ -0,0 +1,22 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Password Reset Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are the default lines which match reasons
+    | that are given by the password broker for a password update attempt
+    | has failed, such as for an invalid token or invalid new password.
+    |
+    */
+
+    'reset' => '¡Su contraseña a sido reestablecida!',
+    'sent' => 'Hemos enviado con correo con un enlace para reestablecer la constraseña',
+    'throttled' => 'Espera antes de reintentar, por favor.',
+    'token' => 'El token de reestablecimiento de contraseña no es válido.',
+    'user' => "No podemos encontrar un usuario con ese correo electrónico.",
+
+];
